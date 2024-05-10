d494610948565d55371b7f899bd92de1bcd378ae
unknown <Sena@MEDAPRCGEFSP578.FormacionSena.RED>
2024-05-10 15:58:16 -0500

    feat; ejercicios condicionales

--- 02-condicionales/index.php
+++ 02-condicionales/index.php
@@ -1,6 +1,41 @@
 <?php
 
-echo 'Ejecutando condicionales...'
+echo 'Ejecutando condicionales...';
+
+/* <!-- ejercicio 1.1 --> */
+
+$Articulos = "25";
+
+if($Articulo >12) {
+    echo "Caja normal";
+} else {
+    echo "Caja rapida";
+}
+
+
+/* <!-- ejercicio 1.2 --> */
+
+$Edad = "25";
+
+if($Edad <12) {
+    echo "Caja normal";
+} else {
+    echo "Caja rapida";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ?>
