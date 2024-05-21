ec8d96b042633bb029f7437868995688d32ec19f
Isabrc12 <irc2234@gmail.com>
2024-05-21 16:13:33 -0500

    feat: avance SQL

---
+++ 07-SQL/index.php
@@ -0,0 +1,27 @@
+<?php
+
+//Documentación: SQL 
+
+//1. SQL SELECT
+//SELECT Nombre, Edad FROM profesores;
+//Selecciona todos los regristros de nombre y edad dentro de la tabla profesores.
+
+//2. SQL SELECT DISTINCT
+//SELECT DISTINCT Grado FROM estudiantes;
+//Selelciona solo un grado y los demás que están repetidos los elimina
+
+//
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
