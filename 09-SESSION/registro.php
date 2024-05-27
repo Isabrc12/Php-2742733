d3571fd8daf6b6011ab8b525a778c51a1b393f45
Isabrc12 <irc2234@gmail.com>
2024-05-27 16:02:01 -0500

    feat: incio sesion

--- 09-SESSION/registro.php
+++ 09-SESSION/registro.php
@@ -1,15 +1,51 @@
-<?php
+<?php session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $usuario = $_POST['user'];
+    $password = $_POST['password'];
 
 
+//Para verificar que se envíen todos los datos
+if(empty($usuario) or empty($password)){
+    echo 'Rellene completo el formulario';
+}else{
+    //echo $usuario . ' - ' . $password;
+    $_SESSION['userRegister'] = $usuario;
+    $_SESSION['passRegister'] = $password;
+    //echo '- Variables de sesión guardadas 🤩';
+    //header('Location: index.php);
 
-if ($_POST['user']) {
-
-    session_start();
-    $_SESSION['nombre'] = $_POST['user'];
-    $_SESSION['password'] = $_POST['password'];
-
-    echo 'Puede iniciar sesión';
-} else {
-    echo 'Tienes que completar el formulario';
-    header('Location: index.php');
 }
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>Registrate</h1>
+
+<form action="registro.php" method="POST">
+    <label for="user">User</label>
+    <input type="text" placeholder="User" name="user">
+    <label for="password">Password</label>
+    <input type="password" placeholder="Password" name="password">
+    <button type="submit">Registrarse</button>
+</form>
+
+<?php if(isset($_SESSION ['userRegister']) ) : ?>
+<p>Datos registrados, ya puede iniciar sesión</p>
+<p><?php echo $_SESSION['userRegister'] . ' - ' . $_SESSION ['passRegister']; ?> </p>
+<a href="index.php">Iniciar sesión</a>
+<?php endif  ?>
+
+</body>
+</html>
