212f40f5da6e94003804704894ff85fc358c8e62
Isabrc12 <irc2234@gmail.com>
2024-05-28 15:49:11 -0500

    feat: base de datos foca

--- 09-SESSION/registro.php
+++ 09-SESSION/registro.php
@@ -3,6 +3,7 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $usuario = $_POST['user'];
     $password = $_POST['password'];
+    $email = $_POST['email'];
 
 
 //Para verificar que se envíen todos los datos
@@ -12,12 +13,25 @@ if(empty($usuario) or empty($password)){
     //echo $usuario . ' - ' . $password;
     $_SESSION['userRegister'] = $usuario;
     $_SESSION['passRegister'] = $password;
+    $_SESSION['emailRegister'] = $email;
     //echo '- Variables de sesión guardadas 🤩';
     //header('Location: index.php);
 
-}
-}
+    try{
 
+        $conexion = new PDO("mysql: host=localhost; dbname=focaapp", 'root', ''); 
+        echo "Conexion OK". "<br>";
+    
+    } catch (PDOException $e) {
+        echo "Error:" . $e-> getMessage() . "<br>";
+    }
+    
+    $statement = $conexion -> prepare("INSERT INTO `usersapp` (`ID` ,`Username`, `Password`,`Email`) VALUES (NULL, :usuario, :pass, :email)");
+    
+    
+    $statement -> execute(array( ":usuario"=>$usuario, ":pass"=>$password, ":email"=>$email));
+}
+}
 
 ?>
 
@@ -36,6 +50,8 @@ if(empty($usuario) or empty($password)){
 <form action="registro.php" method="POST">
     <label for="user">User</label>
     <input type="text" placeholder="User" name="user">
+    <label for="email">Email</label>
+    <input type="email" placeholder="email" name="email">
     <label for="password">Password</label>
     <input type="password" placeholder="Password" name="password">
     <button type="submit">Registrarse</button>
@@ -43,7 +59,7 @@ if(empty($usuario) or empty($password)){
 
 <?php if(isset($_SESSION ['userRegister']) ) : ?>
 <p>Datos registrados, ya puede iniciar sesión</p>
-<p><?php echo $_SESSION['userRegister'] . ' - ' . $_SESSION ['passRegister']; ?> </p>
+<p><?php echo $_SESSION['userRegister'] . ' - ' . $_SESSION ['passRegister'] . ' - ' . $_SESSION ['emailRegister'] ; ?> </p>
 <a href="index.php">Iniciar sesión</a>
 <?php endif  ?>
 
