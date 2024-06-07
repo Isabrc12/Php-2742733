b936aba331d6b42072a7cd8d221897fd1cb7ee59
Isabrc12 <irc2234@gmail.com>
2024-06-07 16:05:33 -0500

    feat: estilos pagina

--- 10-CRUD/index.php
+++ 10-CRUD/index.php
@@ -10,37 +10,79 @@
 
 <body>
 
+    <div>
+        <div id="carouselExampleIndicators" class="carousel slide" style="margin-bottom: 30px;">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="./img/chica1.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="./img/chica2.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="./img/chican3.png" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+
+    <div>
+        <div class="container text-center">
+            <div class="row align-items-start">
+                <div class="col">
+                Nuestros cursos están diseñados para brindarte una experiencia completa y práctica en el mundo del maquillaje. Desde principiantes hasta aquellos con experiencia, ¡tenemos algo para todos! A través de clases interactivas, aprenderás técnicas profesionales, trucos y consejos para perfeccionar tu habilidad y elevar tu juego de maquillaje al siguiente nivel.
+                </div>
+                <div class="col">
+                
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="container">
 
         <div class="container text-center">
             <div class="row align-items-center">
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img src="./img/maquillaje1.png" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <h5 class="card-title">Curso de maquillaje (Nivel basico)</h5>
+                            <p class="card-text">Este curso de maquillaje básico ofrece una introducción completa a las técnicas esenciales, desde la preparación de la piel hasta la aplicación de sombras y labiales.</p>
+                            <a href="#" class="btn btn-secondary">Ver más</a>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img src="./img/maquillaje2.png" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <h5 class="card-title">Curso de maquillaje (Nivel intermedio)</h5>
+                            <p class="card-text">Este curso de maquillaje de nivel intermedio impulsa habilidades avanzadas, guiando a los estudiantes hacia la maestría en técnicas versátiles y expresivas bajo la tutela de instructores expertos.</p>
+                            <a href="#" class="btn btn-secondary">Ver más</a>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img src="./img/maquillaje3.png" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <h5 class="card-title">Curso de maquillaje (Nivel profesional)</h5>
+                            <p class="card-text">El curso de maquillaje profesional ofrece una inmersión completa en técnicas de vanguardia, desde la creación de looks de pasarela hasta el dominio de la aerografía.</p>
+                            <a href="#" class="btn btn-secondary">Ver más</a>
                         </div>
                     </div>
                 </div>
