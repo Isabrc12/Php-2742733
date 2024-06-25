0979fcd6f86f9c35af242b66e744559032c398c3
Isabrc12 <irc2234@gmail.com>
2024-06-25 14:44:25 -0500

    feat:crud

--- 10-CRUD/editar_curso.php
+++ 10-CRUD/editar_curso.php
@@ -21,22 +21,41 @@ if (isset($_GET['id'])) {
     header("Location: curso.php"); */
 }
 
-if(isset ($_POST['actualizar_curso'])){
- /* echo "<h1>Formulario de actualización enviado</h1>"; */
+if (isset($_POST['actualizar_curso'])) {
+    /* echo "<h1>Formulario de actualización enviado</h1>"; */
     $id = $_GET['id'];
     $tituloActualizado = $_POST['titulo'];
     $descripcionActualizado = $_POST['descripcion'];
     $estudiantesActualizado = $_POST['estudiantes'];
 
-    $statement = $conexion->prepare ("UPDATE `cursos` SET `titulo`= ?,`descripcion`= ?,`estudiantes`= ? WHERE id = ?");
-    $statement->execute(array($tituloActualizado, $descripcionActualizado, $estudiantesActualizado, $id));
+    //aqui capturamos datos de la imagen recibida 
+    $imagenCurso = $_FILES['imagenCurso']['tmp_name'];
+    $nombreImagen = $_FILES['imagenCurso']['name'];
+    $tipoImagen = pathinfo($nombreImagen, PATHINFO_EXTENSION);
+    $sizeImg = $_FILES['imagenCurso']['size'];
+    $directorio = "images/";
 
-    header("Location: curso.php");
 
+    if ($tipoImagen == 'jpg' or $tipoImagen == 'jpeg' or $tipoImagen == 'png') {
 
+        $ruta = $directorio . $id . '.' . $tipoImagen;
 
+        $statement = $conexion->prepare("UPDATE `cursos` SET imagen=?, `titulo`= ?,`descripcion`= ?,`estudiantes`= ? WHERE id = ?");
+        $statement->execute(array($ruta, $tituloActualizado, $descripcionActualizado, $estudiantesActualizado, $id));
+
+        if (move_uploaded_file($imagenCurso, $ruta)) {
+            $_SESSION['mensaje'] = 'Curso actualizado exitosamente💄';
+            $_SESSION['color'] = 'success';
+
+            header('Location: curso.php');
+        }
+    } else {
+        $_SESSION['mensaje'] = 'formato no admitido🤰';
+        $_SESSION['color'] = 'danger';
+
+        header('Location: curso.php');
+    }
 }
-
 ?>
 
 <?php require('header.php'); ?>
@@ -44,7 +63,10 @@ if(isset ($_POST['actualizar_curso'])){
 <div class="container">
     <div class="row">
         <div class="col">
-            <form action="editar_curso.php?id=<?php echo $id?>" method="POST">
+            <form action="editar_curso.php?id=<?php echo $id ?>" method="POST" enctype="multipart/form-data">
+
+                <label for="imagen">Imagen del curso</label>
+                <input class="form-control" type="file" name="imagenCurso">
 
                 <label for="titulo">Titulo</label>
                 <input class="form-control" type="text" id="titulo" name="titulo" value="<?php echo  $titulo ?>" required>
@@ -53,7 +75,7 @@ if(isset ($_POST['actualizar_curso'])){
                 <input class="form-control" type="text" id="descripcion" name="descripcion" value="<?php echo  $descripcion ?>" required>
 
                 <label for="estudiantes">Estudiantes</label>
-                <input class="form-control" type="number"  id="estudiantes" name="estudiantes" value="<?php echo  $estudiantes ?>" required>
+                <input class="form-control" type="number" id="estudiantes" name="estudiantes" value="<?php echo  $estudiantes ?>" required>
 
                 <br>
 
